d6acdde0c02afed7be4fc272c7e2f228697d69e1
AlwinJamesM <alwinjamesmueca@gmail.com>
2024-06-21 13:27:47 +0800

    V3

---
+++ about-Us.php
@@ -0,0 +1,124 @@
+<?php $title = 'about-Us'; ?>
+<?php include 'header.php' ?>
+
+<title>About Us</title>
+<!-- body --->
+
+<section
+    class="page-header page-header-modern page-header-background page-header-background-md overlay overlay-color-dark overlay-show overlay-op-7"
+    style="background-image: url(img/bg-myray.jpg);">
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-md-12 align-self-center p-static order-2 text-center">
+                <h1 class="text-9 font-weight-bold">About Us</h1>
+            </div>
+            <div class="col-md-12 align-self-center order-1">
+                <ul class="breadcrumb breadcrumb-light d-block text-center">
+                    <li><a href="myray.php">Home</a></li>
+                    <li class="active">About Us</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+
+<div class="container">
+
+    <div class="row pt-5">
+        <div class="col">
+
+            <div class="row text-center pb-5">
+                <div class="col-md-9 mx-md-auto">
+                    <div class="overflow-hidden mb-3">
+                        <h1 class="word-rotator slide font-weight-bold text-8 mb-0 appear-animation"
+                            data-appear-animation="maskUp">
+                            <span>We are SPMS, We</span>
+                            <span class="word-rotator-words bg-primary">
+                                <b class="is-visible">Create</b>
+                                <b>Build</b>
+                                <b>Develop</b>
+                            </span>
+                            <span> Solutions</span>
+                        </h1>
+                    </div>
+                    <div class="overflow-hidden mb-3">
+                        <p class="lead mb-0 appear-animation" data-appear-animation="maskUp"
+                            data-appear-animation-delay="200">
+                            Since 1989, St. Patrick's Medical Systems (SPMS) has been "Nurturing Growth Solutions" for
+                            the healthcare organizations we deliberately serve.
+                            We empower our organization to continuously be better in achieving our objective by
+                            embracing:
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row mt-3 mb-5">
+                <div class="col-md-4 appear-animation" data-appear-animation="fadeInLeftShorter"
+                    data-appear-animation-delay="800">
+                    <h3 class="font-weight-bold text-4 mb-2">Our Mission</h3>
+                    <p>To be the preferred solutions provider in every healthcare office.</p>
+                </div>
+                <div class="col-md-4 appear-animation" data-appear-animation="fadeIn" data-appear-animation-delay="600">
+                    <h3 class="font-weight-bold text-4 mb-2">Our Vision</h3>
+                    <p>Enabling a successful healthcare practice through better customer solutions and employee
+                        excellence.</p>
+                </div>
+                <div class="col-md-4 appear-animation" data-appear-animation="fadeInRightShorter"
+                    data-appear-animation-delay="800">
+                    <h3 class="font-weight-bold text-4 mb-2">Our Values</h3>
+                    <p>Be Proactive <br>
+                        Think Win-Win <br>
+                        Continuously Be Better <br>
+                        Ownership Mentality <br>
+                        Logical Urgency <br>
+                        Teamwork <br>
+                        Respect <br>
+                        Integrity <br>
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<section class="section section-primary border-0 mb-0 appear-animation" data-appear-animation="fadeIn"
+    data-plugin-options="{'accY': -150}">
+    <div class="container">
+        <div class="row counters counters-sm pb-4 pt-3">
+            <div class="col-sm-6 col-lg-3 mb-5 mb-lg-0">
+                <div class="counter">
+                    <i class="icons icon-user text-color-light"></i>
+                    <strong class="text-color-light font-weight-extra-bold" data-to="45000" data-append="+">0</strong>
+                    <label class="text-4 mt-1 text-color-light">Happy Clients</label>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3 mb-5 mb-lg-0">
+                <div class="counter">
+                    <i class="icons icon-badge text-color-light"></i>
+                    <strong class="text-color-light font-weight-extra-bold" data-to="35">0</strong>
+                    <label class="text-4 mt-1 text-color-light">Years In Business</label>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3 mb-5 mb-sm-0">
+                <div class="counter">
+                    <i class="icons icon-graph text-color-light"></i>
+                    <strong class="text-color-light font-weight-extra-bold" data-to="96" data-append="%">0</strong>
+                    <label class="text-4 mt-1 text-color-light">Ratings</label>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="counter">
+                    <i class="fa-regular fa-user text-color-light"></i>
+                    <strong class="text-color-light font-weight-extra-bold" data-to="300" data-append="+">0</strong>
+                    <label class="text-4 mt-1 text-color-light">Employees</label>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- end body --->
+<?php include 'footer.php' ?>
---
+++ careers-form.php
@@ -0,0 +1,192 @@
+<?php $title = 'careers-form'; ?>
+<?php include 'header.php' ?>
+
+<title>careers form</title>
+
+<div role="main" class="main">
+
+    <section class="page-header page-header-modern custom-page-header-style-1 bg-color-primary page-header-lg mb-0">
+        <div class="container container-xl-custom py-5">
+            <div class="row">
+                <div class="col-md-8 order-2 order-md-1 align-self-center p-static">
+                    <h1 class="font-weight-extra-bold text-14 appear-animation"
+                        data-appear-animation="fadeInRightShorter" data-appear-animation-delay="200">CAREERS</h1>
+                </div>
+                <div class="col-md-4 order-1 order-md-2 align-self-center">
+                    <ul class="breadcrumb d-block text-md-end breadcrumb-light appear-animation"
+                        data-appear-animation="fadeInRightShorter" data-appear-animation-delay="400">
+                        <li><a href="#">Home</a></li>
+                        <li class="active">Careers</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section border-0 py-0 m-0">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-5 px-0">
+                    <div class="d-flex flex-column justify-content-center bg-color-dark h-100 p-5">
+                        <div class="row justify-content-center pt-2 mt-5">
+                            <div class="col-md-9">
+                                <div class="feature-box flex-column flex-xl-row align-items-center align-items-xl-start text-center text-xl-start appear-animation"
+                                    data-appear-animation="fadeInUpShorter" data-appear-animation-delay="600">
+                                    <div class="feature-box-icon bg-color-transparent w-auto h-auto pt-0">
+                                        <img src="img/demos/it-services/icons/building.svg" class="img-fluid" width="95"
+                                            alt="" data-icon
+                                            data-plugin-options="{'onlySVG': true, 'extraClass': 'svg-fill-color-light'}" />
+                                    </div>
+                                    <div class="feature-box-info ps-2 pt-1">
+                                        <h4 class="text-color-light font-weight-bold text-6">St Patrick's Medical System
+                                            Inc.</h4>
+                                        <p
+                                            class="text-color-light opacity-5 font-weight-light custom-text-size-1 pb-2 mb-4">
+                                            3/f East Wing St.,<br>Patrick's square building<br>556 Shaw Boulevard,
+                                            Mandaluyong City</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-9 appear-animation" data-appear-animation="fadeInUpShorter"
+                                data-appear-animation-delay="800">
+                                <hr class="bg-color-light opacity-2">
+                            </div>
+                        </div>
+                        <div class="row justify-content-center py-2">
+                            <div class="col-auto text-center mb-4 mb-sm-0 mb-lg-4 mb-xl-0 appear-animation"
+                                data-appear-animation="fadeInRightShorter" data-appear-animation-delay="1000">
+                                <span class="d-block font-weight-semibold text-center">SUPPORT</span>
+                                <a href="tel:+1234567890" class="text-color-light font-weight-bold text-6">(02) 8533
+                                    8814</a>
+                            </div>
+                            <div class="col-auto text-center ms-xl-5 appear-animation"
+                                data-appear-animation="fadeInRightShorter" data-appear-animation-delay="1200">
+                                <span class="d-block font-weight-semibold text-center">SALES</span>
+                                <a href="tel:+1234567890" class="text-color-light font-weight-bold text-6">(02) 8637
+                                    9694</a>
+                            </div>
+                        </div>
+                        <div class="row justify-content-center pt-2 pb-3 mb-5">
+                            <div class="col-md-9 appear-animation" data-appear-animation="fadeInUpShorter"
+                                data-appear-animation-delay="1400">
+                                <hr class="bg-color-light opacity-2">
+                            </div>
+                            <div class="col-md-12 text-center appear-animation" data-appear-animation="fadeInUpShorter"
+                                data-appear-animation-delay="1600">
+                                <span class="d-block font-weight-semibold text-center">SEND AN EMAIL</span>
+                                <a href="mailto:mail@domain.com"
+                                    class="text-color-light font-weight-bold text-decoration-underline text-5">spmsphilippines@gmail.com</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-7 px-0">
+                    <div class="responsive-iframe">
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15444.932931465306!2d121.04678669364013!3d14.585781629084192!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x59f9878959c314fc!2sSt.%20Patrick's%20Medical%20Systems%20Inc.%20(SPMS)!5e0!3m2!1sen!2sph!4v1571243944683!5m2!1sen!2sph"
+                            frameborder="0" style="border:0;" allowfullscreen="" id="fitvid0"></iframe>
+                    </div>
+                </div>
+            </div>
+    </section>
+
+
+    <section class="section custom-section-full-width bg-color-transparent border-0 mt-1 mb-1">
+        <div class="container container-xl-custom mt-3">
+            <div class="row justify-content-center">
+                <div class="col-lg-10">
+                    <div class="overflow-hidden mb-2">
+                        <span
+                            class="d-block font-weight-bold custom-text-color-grey-1 text-center line-height-1 mb-0 appear-animation"
+                            data-appear-animation="maskUp" data-appear-animation-delay="400">JOB APPLICATION</span>
+                    </div>
+                    <div class="overflow-hidden mb-4">
+                        <h2 class="text-color-dark font-weight-bold text-center text-8 line-height-2 mb-0 appear-animation"
+                            data-appear-animation="maskUp" data-appear-animation-delay="600">APPLY NOW!</h2>
+                    </div>
+                    <p class="custom-text-size-1 text-center mb-5 appear-animation"
+                        data-appear-animation="fadeInUpShorter" data-appear-animation-delay="800">Fill up form to
+                        schedule your application with our team.</p>
+                </div>
+            </div>
+            <div class="col-lg-9 order-1 order-lg-2">
+
+                <div class="overflow-hidden mb-1">
+                    <h2 class="font-weight-normal text-7 mb-0"><strong class="font-weight-extra-bold">Application
+                            Form</strong></h2>
+                </div>
+
+                <form class="contact-form-recaptcha-v3" action="php/contact-form-recaptcha-v3.php" method="POST"
+                    novalidate="novalidate">
+                    <div class="contact-form-success alert alert-success d-none mt-4">
+                        <strong>Success!</strong> Your message has been sent to us.
+                    </div>
+
+                    <div class="contact-form-error alert alert-danger d-none mt-4">
+                        <strong>Error!</strong> There was an error sending your message.
+                        <span class="mail-error-message text-1 d-block"></span>
+                    </div>
+
+                    <div class="row">
+                        <div class="form-group col-lg-6">
+                            <label class="form-label mb-1 text-2">Full Name</label>
+                            <input type="text" value="" data-msg-required="Please enter your name." maxlength="100"
+                                class="form-control text-3 h-auto py-2" name="name" required="">
+                        </div>
+                        <div class="form-group col-lg-6">
+                            <label class="form-label mb-1 text-2">Email Address</label>
+                            <input type="email" value="" data-msg-required="Please enter your email address."
+                                data-msg-email="Please enter a valid email address." maxlength="100"
+                                class="form-control text-3 h-auto py-2" name="email" required="">
+                        </div>
+                        <div class="form-group col-lg-6">
+                            <label class="form-label mb-1 text-2">Contact No.</label>
+                            <input type="text" value="" data-msg-required="Please enter phone number." maxlength="100"
+                                class="form-control text-3 h-auto py-2" phonenumber="name" required="">
+                        </div>
+                        <div class="form-group col-lg-6">
+                            <label class="form-label mb-1 text-2">Birthdate</label>
+                            <input type="date" value="" data-msg-required="Please enter birthdate." maxlength="100"
+                                class="form-control text-3 h-auto py-2" birthdate="birthdate" required="">
+                        </div>
+                        <div class="form-group col-lg-6">
+                            <label class="form-label mb-1 text-2">Address</label>
+                            <input type="text" value="" data-msg-required="Please enter phone number." maxlength="100"
+                                class="form-control text-3 h-auto py-2" phonenumber="name" required="">
+                        </div>
+
+                        <div class="form-group col-lg-6">
+                            <label class="form-label mb-1 text-2" for="position">Position</label><br>
+                            <select id="position" name="position" class="form-control text-3 h-auto py-2">
+                                <option value="Select position">Select position:</option>
+                                <option value="cluster sale">Cluster Sale Executive</option>
+                                <option value="waregouse officer">Warehouse And Logistics Officer</option>
+                                <option value="ece">Electronics And Communications Engineer</option>
+                                <option value="service technician">Service Technician</option>
+                                <option value="technical sales">Technical Sales Specialist</option>
+                                <option value="it associate">IT Associate - Software</option>
+                            </select>
+                        </div>
+
+                        <div class="form-group col-lg-6">
+                            <label class="form-label mb-1 text-2" for="template-jobform-cvfile">Upload CV (PDF or
+                                DOC)</label>
+                            <input type="file" id="template-jobform-cvfile" name="template-jobform-cvfile" value=""
+                                class="required sm-form-control valid" accept="application/msword, application/pdf">
+                        </div>
+
+                    </div>
+
+                    <div class="form-group col">
+                        <input type="submit" value="Submit form" class="btn btn-primary btn-modern"
+                            data-loading-text="Loading...">
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </section>
+</div>
+
+
+<?php include 'footer.php' ?>
